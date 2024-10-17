4bf69383a30f952f40f981c9830c3d957b3ddd74
cristiandavid0124 <cristian.naranjo@mail.escuelaing.edu.co>
2024-10-16 20:55:54 -0500

    agregando otra ves

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import  com.example.demo.service.UserService;
+import com.example.demo.service.UserService;
 
 /**
  * The LoginController class handles user authentication and registration.
